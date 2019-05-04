82b4b428680c7b99850209121292a0850bc16c1a
Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2019-05-04 14:36:37 -0400

    repr

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_param(i):
     value is not string, returns None.
     """
     try:
-        raise Exception(request.data.decode('utf-8'))
+        raise Exception(repr(request.data))
         req_body = json.loads(request.data.decode('utf-8'))
         if not req_body or i not in req_body:
             return None
